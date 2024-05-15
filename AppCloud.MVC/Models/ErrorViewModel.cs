585b59e91468f0d052080e62f7c160e3ee2beddb
victorees <veecheverrias@utn.edu.ec>
2024-05-15 12:08:21 -0500

    MVC aplicado

---
+++ AppCloud.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppCloud.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
